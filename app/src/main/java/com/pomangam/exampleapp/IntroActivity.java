d06443e7c813952598fe4181313b33b0f1e80fe1
CHOI\Choi <cholnh1@naver.com>
2019-05-13 20:58:39 +0900

    Initial commit

---
+++ app/src/main/java/com/pomangam/exampleapp/IntroActivity.java
@@ -0,0 +1,46 @@
+package com.pomangam.exampleapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private Handler handler;
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_intro);
+        init();
+
+        handler.postDelayed(runnable, 1500);
+    }
+
+    public void init() {
+        handler = new Handler();
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        handler.removeCallbacks(runnable);
+    }
+}
